0ab435427bcf7be365abeb6073661158bd6e63c3
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 19:54:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,6 @@
 # Write your methods here
+require pry
+
 def loop_message_five_times(message)
   5.times{puts message}
 end
@@ -6,7 +8,7 @@ end
 def loop_message_n_times(message, number)
   number.times{puts message.to_i}
 end
-
+binding.pry
 def output_array(array)
     counter = 0
     i = 0
@@ -14,4 +16,4 @@ def output_array(array)
     puts array[i]
     counter++
     i++
-  end 
+  end
